5336196f0bc43c9353e0eb2e73ab42b61d735688
crypto-gitty <125034920+crypto-gitty@users.noreply.github.com>
2023-07-21 10:17:42 +0200

    Token Contact Address is updated

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x833c069058fce3e43102c055cee614055816bed0";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
